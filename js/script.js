6828741aa743b60ed68ec2c573040e6c7877a3b3
Pedro Andrade <pedrotrabalhos123@hotmail.com>
2025-11-21 17:08:00 -0300

    Finalizando o projeto, ajustando a responsividade geral da página e adicionando script Js para formatação e validação dos campos do formulário

--- js/script.js
+++ js/script.js
@@ -9,3 +9,49 @@ function abrirMenu() {
     document.querySelector(".hamburguer").src = "./imgs/Fechar-menu.svg";
   }
 }
+
+// Formatação celular
+
+const form = document.querySelector(".form");
+const inputCelular = document.getElementById("celular");
+const msgErro = document.getElementById("msgErro");
+
+inputCelular.addEventListener("input", function (e) {
+  let valor = e.target.value;
+
+  valor = valor.replace(/\D/g, "");
+
+  if (valor.length > 11) {
+    valor = valor.slice(0, 11);
+  }
+
+  if (valor.length > 2) {
+    valor = valor.replace(/^(\d{2})(\d)/g, "($1) $2");
+  }
+  if (valor.length > 10) {
+    valor = valor.replace(/(\d)(\d{4})$/, "$1-$2");
+  }
+
+  e.target.value = valor;
+
+  inputCelular.classList.remove("input-erro");
+  msgErro.style.display = "none";
+});
+
+form.addEventListener("submit", function (e) {
+  const numeroLimpo = inputCelular.value.replace(/\D/g, "");
+
+  if (numeroLimpo.length < 11) {
+    e.preventDefault();
+
+    inputCelular.classList.add("input-erro");
+    msgErro.style.display = "block";
+
+    inputCelular.focus();
+  }
+});
+
+// Animação da página
+if (window.SimpleAnime) {
+  new SimpleAnime();
+}
